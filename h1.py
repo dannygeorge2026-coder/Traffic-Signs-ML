809b7ff25b9e3942bc5ebfaf4ec4d7d084890e37
dannygeorge2026-coder <dannygeorge2026@gmail.com>
2026-06-08 16:39:56 +0530

    Delete h1.py

--- h1.py
+++
@@ -1,4 +0,0 @@
-import torch
-print(torch.cuda.is_available())   # should be True
-print(torch.version.cuda)          # should show 13.0
-print(torch.cuda.get_device_name(0))  # should show "NVIDIA GeForce RTX 3050"
